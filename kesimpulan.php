34c353e380d0de1d99c09d2eb839ca4c1111176d
IMAMSODIK <40739580+IMAMSODIK@users.noreply.github.com>
2022-05-12 01:35:50 +0700

    Update kesimpulan.php

--- kesimpulan.php
+++ kesimpulan.php
@@ -27,9 +27,9 @@
     <h1>Perbedaan antara include dan require</h1>
  	<div class="content">
         <p>Jika menggunakan include, saat terjadi kesalahan, maka hanya akan menghasilkan peringatan saja dan program akan terus dijalankan seperti pada gambar di bawah.</p>
-        <img src="assets/include.png">
+        <img src="aset/include_ex.png">
         <p>Berbeda jika menggunakan require, program akan menghasilkan pesan "fatal-error" dan program di bawahnya tidak akan di eksekusi lagi seperti pada gambar di bawah.</p>
-        <img src="assets/require.png">
+        <img src="aset/require_ex.png">
     </div>
 
  </body>
